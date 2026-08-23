b0978a54a2bfdbf5ddb7dfe7667f8a8bb2f22732
BoatMonkey678 <koxipro@gmail.com>
2026-08-23 08:58:35 +0200

    Added media and config support

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import configparser
+
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
@@ -7,8 +9,15 @@ from fastapi.templating import Jinja2Templates
 
 from html_objects import *
 
+config = configparser.ConfigParser()
+config.read("config.ini")
+server_host = config["server"]["host"]
+server_port = config.getint("server", "port")
+api_url = f"http://{server_host}:{server_port}"
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/resources", StaticFiles(directory="resources"), name="resources")
 templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(
@@ -24,15 +33,15 @@ answered_questions: dict[int, str] = {}
 # Serving HTMLs
 @app.get("/", response_class=HTMLResponse)
 async def serve_index(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
+    return templates.TemplateResponse(request=request, name="index.html", context={"api_url": api_url})
 
 @app.get("/about", response_class=HTMLResponse)
 async def serve_about(request: Request):
-    return templates.TemplateResponse(request=request, name="about.html")
+    return templates.TemplateResponse(request=request, name="about.html", context={"api_url": api_url})
 
 @app.get("/question", response_class=HTMLResponse)
 async def serve_question(request: Request):
-    return templates.TemplateResponse(request=request, name="question.html")
+    return templates.TemplateResponse(request=request, name="question.html", context={"api_url": api_url})
 
 @app.post("/reset")
 async def reset_answers():
@@ -44,16 +53,16 @@ async def reset_answers():
 # Sample: generates some question.
 @app.get("/question/get", response_model=Question)
 async def get_question():
-    return Question(id=100, description="Sample Question", answers={"A": "Answer A", "B": "Answer B", "C": "Answer C"}, media=None, correct_answer="C")
+    return Question(id=100, description="Sample Question", answers={"A": "Answer A", "B": "Answer B", "C": "Answer C"}, media="BigBuckBunny_320x180.mp4", correct_answer="C")
 
 @app.post("/question/submit-answer")
 async def submit_answer(result: QuestionResult):
     answered_questions[result.id] = result.answer
-    return {"passed": answered_questions}
+    return {"answered": answered_questions}
 
 if __name__ == "__main__":
     uvicorn.run(
         app,
-        host="127.0.0.1",
-        port=8000,
+        host=server_host,
+        port=server_port,
     )
